82f5f5b7b8466ed4fd37b218415eb6f8ad81d215
roliynyk <roliynyk@uccs.edu>
2019-03-28 15:08:46 -0600

    tkinter aded

---
+++ gui.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import messagebox
+
+def helloCallBack():
+   messagebox.showinfo( "Hello Python", "Hello World")
+
+master = tkinter.Tk()
+
+C = tkinter.Canvas(master, bg=None, height=900, width=900)
+
+newCharacterButton = tkinter.Button(master, text ="New", command = helloCallBack)
+
+newCharacterButton.pack()
+C.pack()
+master.mainloop()
