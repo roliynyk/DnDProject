3fa41715fe919106d9a3a73e40ac9940afd0d4ce
roliynyk <roliynyk@uccs.edu>
2019-05-03 00:42:34 -0600

    class work

--- LoadData.py
+++ LoadData.py
@@ -6,12 +6,13 @@ class DataDictionary():
         self.races = apicalls.getRaceData()
         self.classes = apicalls.getClassData()
         self.alignments = ["Lawful Good","Neutral Good","Chaotic Good","Lawful Neutral","Neutral","Chaotic Neutral","Lawful Evil","Neutral Evil","Chaotic Evil"]
-
+        self.classNameDict = {i['name']:i['index']-1 for i in self.classes}
+        self.raceNameDict = {i['name']:i['index']-1 for i in self.races}
 #     def retNames(self, var):
 #         for i in var:
 #             print(i["name"])
 
 
-
+print(DataDictionary().raceNameDict)
 # data = DataDictionary()
 # data.retNames(data.classes)
--- NewCharacterGui.py
+++ NewCharacterGui.py
@@ -2,12 +2,6 @@ import tkinter as tk
 from tkinter import *
 import LoadData
 
-'''
-TODO: This is Ruslan's Code for New Char Gui merged with Hunters. Everything besides infoBox works properly. 
-Spent hours trying to fix infoBox in here with no luck :(
-
-'''
-
 class NewCharGui(tk.Tk):
 
     def __init__(self, data):
@@ -26,17 +20,17 @@ class NewCharGui(tk.Tk):
         dropMenu.pack()
         dropMenu.place(relx=0.01, rely=0.05, width=130, height=30)
 
-    def classSelection(self,C):
+    def classSelection(self, C):
         classSelection = []
         for i in self.classes:
             classSelection.append(i['name'])
 
         tk.Label(C, text="Choose a Class").place(relx=0.02, rely=0.14)
-        self.var2 = tk.StringVar(C)
-        self.var2.set(classSelection[0])
-        drop = OptionMenu(C, self.var2, classSelection[0], *classSelection[1:])
-        drop.pack()
-        drop.place(relx=0.01, rely=0.18, width=130, height=30)
+        self.classVar = tk.StringVar(C)
+        self.classVar.set(classSelection[0])
+        classMenu = OptionMenu(C, self.classVar, classSelection[0], *classSelection[1:], command = self.infoBoxPop)
+        classMenu.pack()
+        classMenu.place(relx=0.01, rely=0.18, width=130, height=30)
 
     # creates the info box when things are selected in the gui
     def infoBox(self, C):
@@ -47,9 +41,35 @@ class NewCharGui(tk.Tk):
     def infoBoxPop(self, var):
         self.listBox.delete(0, 'end')
         if isinstance(var, str):
-           info = self.writeRaceData(var)
-           for i in info:
-               self.listBox.insert(END, i)
+            if var in self.raceDic:
+                info = self.writeRaceData(var)
+            elif var in self.classDic:
+                info = self.writeClassData(var)
+            for i in info:
+                self.listBox.insert(END, i)
+
+    def writeClassData(self, val):
+
+        hitDice = str(self.classes[self.classDic[val]]['hit_die'])
+        proficiencyChoice = "Proficiency Choices: "
+        for i in self.classes[self.classDic[val]]['proficiency_choices']:
+            print(i)
+            #proficiencyChoice += i
+
+        savingThrows = "Saving Throws: "
+        for i in self.classes[self.classDic[val]]['saving_throws']:
+            if i != self.classes[self.classDic[val]]['saving_throws'][-1]:
+                savingThrows += i['name'] +', '
+            else:
+                savingThrows += i['name']
+
+        startingEquipment = ''
+        classLevels =''
+        classInfo = ["Class: " + val, "Hit Die: D" + hitDice,
+                     savingThrows,
+                     proficiencyChoice]
+
+        return classInfo
 
     # Populates information about the race in the GUI
     def writeRaceData(self, val):
@@ -83,12 +103,12 @@ class NewCharGui(tk.Tk):
 
     def alignmentSelection(self, C):
         tk.Label(C, text="Select Your Alignment").place(relx=0.02, rely=0.26)
-        self.dropVar = StringVar(C)
-        self.dropVar.set(self.alignments[0])  # default choice
-        dropMenu = OptionMenu(C, self.dropVar, *self.alignments)
-        dropMenu.place(relx=0.01, rely=0.3, width=130, height=30)
+        self.alignmentVar = StringVar(C)
+        self.alignmentVar.set(self.alignments[0])  # default choice
+        alignmentMenu = OptionMenu(C, self.alignmentVar, *self.alignments)
+        alignmentMenu.place(relx=0.01, rely=0.3, width=130, height=30)
 
-    # Subraces is populated based on race selection
+    # Sub-races is populated based on race selection
     def subracesSelection(self, C):
         subrace = ["subraces", "get", "called", "depending", "on race"]
         self.dropVar = StringVar(C)
@@ -100,9 +120,9 @@ class NewCharGui(tk.Tk):
     def levelSelection(self, C):
         # Might need to make this accept xp or 1-20 value for level
         tk.Label(C, text="Enter Level").place(relx=0.8, rely=0.01)
-        listBox = tk.Text(C)
-        listBox.pack()
-        listBox.place(relx=0.80, rely=0.05, width=60, height=22)
+        self.levelText = tk.Text(C)
+        self.levelText.pack()
+        self.levelText.place(relx=0.80, rely=0.05, width=60, height=22)
 
     # Sets the Characters HP stats
     def hitPoints(self, C):
@@ -129,11 +149,13 @@ class NewCharGui(tk.Tk):
         newCharacterWindow.title("New Character Window")
         C = tk.Canvas(newCharacterWindow, bg=None, height=500, width=500)
         C.pack()
-        self.raceDic = {'Dwarf': 0, 'Elf': 1, 'Halfling': 2, 'Human': 3, 'Dragonborn': 4, 'Gnome': 5, 'Half-Elf': 6,
-                        'Half-Orc': 7, 'Tiefling': 8}
+
+        self.raceDic = self.data.raceNameDict
+        self.classDic = self.data.classNameDict
         self.races = self.data.races
         self.classes = self.data.classes
         self.alignments = self.data.alignments
+
         self.raceSelection(C)
         self.classSelection(C)
         self.createChar(C)
@@ -148,5 +170,8 @@ class NewCharGui(tk.Tk):
         newCharacterWindow.mainloop()
 
     def getSelectionInfo(self):
-        #Example on how to get new selection info, can be used to index into character info dict from api
-        print("Selections: " + self.dropVar.get() + ", " + self.var2.get())
+        # Example on how to get new selection info, can be used to index into character info dict from api
+        print("Selections: " + self.dropVar.get())
+        print("Class: " + self.classVar.get())
+        print("Alignment: "+self.alignmentVar.get())
+        print("level: "+self.levelText.get('1.0',END))
